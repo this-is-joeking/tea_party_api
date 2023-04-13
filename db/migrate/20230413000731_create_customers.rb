bb29b5f4ee6bfdb62f29c369e4c53bf383e17d7f
Joe King <j.king92@gmail.com>
2023-04-12 17:10:23 -0700

    chore: Add db models and migrations

---
+++ db/migrate/20230413000731_create_customers.rb
@@ -0,0 +1,17 @@
+class CreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :address_line1
+      t.string :address_line2
+      t.string :city
+      t.string :state
+      t.string :postal_code
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230413000755_create_teas.rb
@@ -0,0 +1,11 @@
+class CreateTeas < ActiveRecord::Migration[5.2]
+  def change
+    create_table :teas do |t|
+      t.string :title
+      t.string :description
+      t.integer :brewtime
+
+      t.timestamps
+    end
+  end
+end
